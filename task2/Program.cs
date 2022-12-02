078186e71a75dbd0e6552bec17d5cda52f4e7676
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-02 20:58:45 +0200

    Task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// найти большее 
+// разделить большее на меньшее и проверить является ли кратным
+// если 
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if(a % b == 0)
+    {
+        Console.WriteLine("Одно число кратно другому");
+    }
+    else 
+    {
+        int result = a % b;
+        Console.WriteLine("Не кратно и остаток равен " + result);
+    }
+}
+if (b > a)
+{
+    if(b % a == 0)
+    {
+        Console.WriteLine("Одно число кратно другому");
+    }
+    else 
+    {
+        int result = b % a;
+        Console.WriteLine("Не кратно и остаток равен " + result);
+    }
+}
