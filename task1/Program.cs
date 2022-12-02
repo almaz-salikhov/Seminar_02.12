3f48ff9a3c583033584a243407ae716f9388c138
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-02 20:38:53 +0200

    Task 1 done

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿//1. генерируем число из указанного диапазона 
+//2. получаем первую цифру этого числа 
+//3. получаем вторую цифру этого числа 
+//4. присваиваем max первому для определенности сравниваем цифры между собой, выяв
+//
+
+int N = new Random().Next(10, 99);
+Console.WriteLine(N);
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+int max = 0;
+
+if(firstDigit < secondDigit)
+{
+    Console.WriteLine(secondDigit);
+}
+else 
+{
+    Console.WriteLine(firstDigit);
+}
+if (firstDigit == secondDigit)
+{
+    Console.WriteLine("Обе цифры равны");
+}
+//можно добавить обработку случая когда цифры равны
