5051e188cc89e4c54c5fd377f1380f2f9b148b3f
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-03 16:17:28 +0200

    HW task2 solved but the optimization question remains

--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// вопрос: как описать все возможные ситуации с разницей количества знаков, не делая это вручную, вводя каждый кейс до бесконечности? 
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N >= 100)
+{
+    if(N <= 999)
+    {
+        int result = N % 10;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 9999)
+    {
+        int result = N % 100 / 10;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 99999)
+    {
+        int result = N % 1000 / 100;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 999999)
+    {
+        int result = N % 10000 / 1000;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 9999999)
+    {
+        int result = N % 100000 / 10000;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 99999999)
+    {
+        int result = N % 1000000 / 100000;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+    else if(N <= 999999999)
+    {
+        int result = N % 10000000 / 1000000;
+        Console.Write("Третья цифра заданного числа: ");
+        Console.WriteLine(result);
+    }
+   
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
+
+
